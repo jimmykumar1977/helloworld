e36a77433def3361fdf1906b891da4598784f27a
jimmy kumar <jimmy.kumar@gmail.com>
2020-07-04 13:39:06 +0530

    commen added

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		 // demo application to test jenkins pipeline
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
